23bbb3413f5d26c794f7ad2eb194debaa3d91c44
Gerard <gerard.dziwinski@infotech.edu.pl>
2024-12-17 15:41:40 +0100

    Zadanie 62.2

--- ZadaniaCKE/Zadanie62/Zadanie62.py
+++ ZadaniaCKE/Zadanie62/Zadanie62.py
@@ -3,6 +3,11 @@ with open("liczby1.txt", "r") as plik:
     for linia in plik.readlines():
         lista_liczb1.append(linia.strip())
 
+lista_liczb2 = []
+with open("liczby2.txt", "r") as plik:
+    for linia in plik.readlines():
+        lista_liczb2.append(linia.strip())
+
 def PierwszeZadanie():
     max_d = 0
     min_d = int(lista_liczb1[0].strip())
@@ -28,3 +33,30 @@ def PierwszeZadanie():
 
 print(PierwszeZadanie())
 
+def DrugieZadanie():
+    start_max = 0
+    ciag_max = 0
+    poprzedni = 0
+
+    ciag_teraz = 0
+    start_teraz = lista_liczb2[0]
+    for i in range(len(lista_liczb2)):
+        if int(lista_liczb2[i]) >= poprzedni:
+            ciag_teraz += 1
+            poprzedni = int(lista_liczb2[i])
+        else:
+            if ciag_teraz > ciag_max:
+                ciag_max = ciag_teraz
+                start_max = lista_liczb2[i-ciag_teraz]
+
+            ciag_teraz = 1
+            start_teraz = lista_liczb2[i]
+            poprzedni = int(lista_liczb2[i])
+
+    if ciag_teraz > ciag_max:
+        ciag_max = ciag_teraz
+        start_max = start_teraz
+
+    return start_max, ciag_max
+
+print(DrugieZadanie())
